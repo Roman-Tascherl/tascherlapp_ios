0c2867cfd3bc43897cec8109ddc726c05f6cce10
Roman-Tascherl <rrafetseder@student.tgm.ac.at>
2026-06-24 15:00:27 +0200

    Initial Commit

---
+++ Tascherl/TascherlApp.swift
@@ -0,0 +1,17 @@
+//
+//  TascherlApp.swift
+//  Tascherl
+//
+//  Created by Roman Rafetseder on 24.06.26.
+//
+
+import SwiftUI
+
+@main
+struct TascherlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
